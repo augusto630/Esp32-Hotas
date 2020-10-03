77ba15cb7e06093ac46a0cca1401023fe7d11315
Augusto <>
2020-10-03 17:03:50 +0100

    Add project files.

---
+++ ThrottleStick/ThrottleStick.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ThrottleStick.ino
+ Created:	10/3/2020 4:59:43 PM
+ Author:	Augusto
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
